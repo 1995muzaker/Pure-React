d6b677e8cd6780bb058816b26320df62a055085e
Shaikh Muzaker <shaikhmuzaker@gmail.com>
2019-03-05 20:27:27 +0530

    Envelop using props in proptypes-ex

--- proptypes-exercises/src/index.js
+++ proptypes-exercises/src/index.js
@@ -4,23 +4,48 @@ import "./index.css";
 
 var person = {
     name: "Shaikh Muzaker",
+    nameTwo: "Chandu",
     address: {
       street: "27th main V.G.S Layout",
       city: "Bangalore Karnataka",
-      pinCode: "632 311"
+      pinCode: "632 311",
+    },
+    addressTwo: {
+        street: "25th main CN Layout",
+        city: "Chittor AP",
+        pinCode: "723 112",
     },
   };
   
-
-function AddressLabel({ myaddress }) {
-    // const {street, city, pinCode} = address;
+  function Envelop({ myAddress }) {
   return (
     <div className="my-address">
-        <p>{myaddress.name}</p>
-        <p>{myaddress.address.street}.</p>
-        <p>{myaddress.address.city} , {myaddress.address.pinCode}</p>
+        <AddressLabel toPerson={myAddress} /><Stamp />
+        <AddressLabelTwo fromPerson={myAddress}/>
+    </div>
+  );
+}
+function AddressLabel({ toPerson }) {
+  return (
+    <div className="to-person">
+        <p>{toPerson.nameTwo}</p>
+        <p>{toPerson.addressTwo.street}.</p>
+        <p>{toPerson.addressTwo.city} , {toPerson.addressTwo.pinCode}</p>
     </div>
   );
 }
 
-ReactDOM.render(<AddressLabel myaddress={person} />, document.querySelector("#root"));
+const AddressLabelTwo = ({ fromPerson }) => (
+    <div className="from-person">
+        <p>{fromPerson.name}</p>
+        <p>{fromPerson.address.street}.</p>
+        <p>{fromPerson.address.city} , {fromPerson.address.pinCode}</p>
+    </div>
+);
+
+const Stamp = () => (
+   <div className="stamp">stamp</div>
+);
+
+
+ReactDOM.render(<Envelop myAddress={person} />, document.querySelector("#root"));
